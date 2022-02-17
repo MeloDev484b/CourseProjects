4db06e95b2bc103f73f48647e4a9d2f06b1cf739
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-02-17 00:49:24 -0500

    :snake:
    Adjusted formatting, variable names, and function names to be more in line with the PEP 8 style guide.

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -1,47 +1,82 @@
 import random
 from typing import Set
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 
+    'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o', 
+    'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 
+    'z', 'A', 'B', 'C', 'D', 
+    'E', 'F', 'G', 'H', 'I', 
+    'J', 'K', 'L', 'M', 'N', 
+    'O', 'P', 'Q', 'R', 'S', 
+    'T', 'U', 'V', 'W', 'X', 
+    'Y', 'Z'
+]
+
+numbers = [
+    '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9'
+]
+
+symbols = [
+    '!', '#', '$', '%', '&',
+    '(', ')', '*', '+'
+]
 
 password = []
 
-def GenerateLetter():
-    randomLetter = random.randint(0, len(letters)-1)
-    return letters[randomLetter]
-def GenerateNumber():
-    randomNumber = random.randint(0, len(numbers)-1)
-    return numbers[randomNumber]
-def GenerateSymbol():
-    randomSymbol = random.randint(0, len(symbols)-1)
-    return symbols[randomSymbol]
-def AddLetters(limit):
+
+def generate_letter():
+    random_letter = random.randint(0, len(letters)-1)
+    return letters[random_letter]
+
+
+def generate_number():
+    random_number = random.randint(0, len(numbers)-1)
+    return numbers[random_number]
+
+
+def generate_symbol():
+    random_symbol = random.randint(0, len(symbols)-1)
+    return symbols[random_symbol]
+
+
+def add_letters(limit):
     letter = int(input("How many letters would you like to use in your password?\n"))
     if letter > limit:
         letter = limit
     for letter in range(0, letter):
         if len(password) < limit:
-            password.append(GenerateLetter())
-def AddNumbers(limit):
+            password.append(generate_letter())
+
+
+def add_numbers(limit):
     if len(password) < limit:
         number = int(input("How many numbers would you like to use in your password?\n"))
         for number in range(0, number):
             if len(password) < limit:
-                password.append(GenerateNumber())
-def AddSymbols(limit):
+                password.append(generate_number())
+
+
+def add_symbols(limit):
     if len(password) < limit:
         symbol = int(limit - len(password))
         for symbol in range(0, symbol):
-            password.append(GenerateSymbol())
-def GeneratePassword(limit):
-    AddLetters(limit)
-    AddNumbers(limit)
-    AddSymbols(limit)
+            password.append(generate_symbol())
+
+
+def generate_password(limit):
+    add_letters(limit)
+    add_numbers(limit)
+    add_symbols(limit)
+
 
 limit = int(input("How many characters would you like your password to contain?\n"))
-GeneratePassword(limit)
+generate_password(limit)
 random.shuffle(password)
-passwordString = ''.join(password)
-print(f"Your password is:\n\n{passwordString}\n")
+password_string = ''.join(password)
+print(f"Your password is:\n\n{password_string}\n")
 
 input("Press enter to exit.\n")
